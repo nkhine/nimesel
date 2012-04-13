49b5e3446d93dffe6c472baa3bb11b8c6d9c0fe2
Mr McHugh <developer@yrucalling.me>
2012-04-13 22:03:32 +1000

    Migrate to webapp2

--- app/app.py
+++ app/app.py
@@ -1,32 +1,15 @@
 # -*- coding: utf-8 -*-
-import os
-from google.appengine.dist import use_library
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-use_library('django', '1.2')
 
-from google.appengine.ext import webapp
+# Import the application framework
+import webapp2
 from google.appengine.ext.webapp.util import run_wsgi_app
-
-# Load request handlers
-import handlers
 from tools.common import is_testenv
 
-# Map url's to handlers
-urls = [
-    (r'/', handlers.Main),
-    (r'/login', handlers.LogIn),
-    (r'/_ah/login_required', handlers.LogIn),
-    (r'/logout', handlers.LogOut),
-    (r'/account', handlers.Account),
-    (r'/account/setup', handlers.AccountSetup),    
-    (r'.*', handlers.Error404)
-]
+# Import our application settings/configuration
+from settings import config
 
-application = webapp.WSGIApplication(urls, debug=is_testenv())
+# And of course import our application routes
+import routes
 
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
+application = webapp2.WSGIApplication(routes.urls, config=config, debug=is_testenv())
+    
